e7767fd14f3b29635c0b8ab331f04f1426a0f4f8
minhalcollege <minhalcollege2017@gmail.com>
2017-10-22 18:03:26 +0300

    fin

--- app/src/main/java/minhal/tomerbu/edu/colorpickers/ColorPickerActivity.java
+++ app/src/main/java/minhal/tomerbu/edu/colorpickers/ColorPickerActivity.java
@@ -3,6 +3,8 @@ package minhal.tomerbu.edu.colorpickers;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +12,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ColorPickerActivity extends AppCompatActivity {
+public class ColorPickerActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, TextWatcher {
     SeekBar sbRed, sbGreen, sbBlue; //Null
     //properties:    //Views
     Button myBtnTst;
@@ -21,6 +23,15 @@ public class ColorPickerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);        //Choose the xml file to inflate (Load)
+        initLayout();
+
+        sbRed.setOnSeekBarChangeListener(this);
+
+        etRed.addTextChangedListener(this);
+
+    }
+
+    private void initLayout() {
         tvResult = (TextView) findViewById(R.id.tvResult);
         myBtnTst = (Button) findViewById(R.id.btnToast);
         etRed = (EditText) findViewById(R.id.etRed);
@@ -29,26 +40,59 @@ public class ColorPickerActivity extends AppCompatActivity {
         sbRed = (SeekBar) findViewById(R.id.sbRed);
         sbGreen = (SeekBar) findViewById(R.id.sbGreen);
         sbBlue = (SeekBar) findViewById(R.id.sbBlue);
-        myBtnTst.setOnClickListener(new View.OnClickListener() {        //after find view by id we can work:
-            @Override
-            public void onClick(View view) {
-                int redProgress = sbRed.getProgress(); //123
-                etRed.setText(String.valueOf(redProgress));
+    }
 
-                int greenProgress = sbGreen.getProgress();
-                etGreen.setText(String.valueOf(greenProgress)); //129
+    private void changeBackground() {
+        int redProgress = sbRed.getProgress(); //123
+        etRed.setText(String.valueOf(redProgress));
 
-                int blueProgress = sbBlue.getProgress();
-                etBlue.setText(String.valueOf(blueProgress)); //0
+        int greenProgress = sbGreen.getProgress();
+        etGreen.setText(String.valueOf(greenProgress)); //129
 
-                int totalColor = Color.rgb(redProgress, greenProgress, blueProgress);
-                tvResult.setBackgroundColor(totalColor);
+        int blueProgress = sbBlue.getProgress();
+        etBlue.setText(String.valueOf(blueProgress)); //0
 
-                String hexColor = Integer.toHexString(totalColor);
-                tvResult.setText(String.format("#%s", hexColor.toUpperCase()));
+        int totalColor = Color.rgb(redProgress, greenProgress, blueProgress);
+        tvResult.setBackgroundColor(totalColor);
 
-                //
-            }
-        });
+        String hexColor = Integer.toHexString(totalColor);
+        tvResult.setText(String.format("#%s", hexColor.toUpperCase()));
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        changeBackground();
+    }
+
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        String redText = etRed.getText().toString();
+        Integer redVal = 0;
+
+        try {
+            redVal = Integer.valueOf(redText);
+        } catch (NumberFormatException e) {
+            etRed.setText("0");
+        }
+
+        sbRed.setProgress(redVal);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+    @Override
+    public void afterTextChanged(Editable editable) {
+
     }
 }
